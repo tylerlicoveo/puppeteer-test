44a06bd12d8947e4c6c98cd77b7efb8e5bcf858c
tylerli1 <tli@coveo.com>
2019-07-23 16:55:11 -0400

    Added render example

---
+++ render.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+const url = process.argv[2];
+
+async function ssr(url) {
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+    const page = await browser.newPage();
+
+    await page.goto(url, {
+        waitUntil: 'networkidle0'
+    });
+
+    const html = await page.content(); // serialized HTML of page DOM
+    await browser.close();
+
+    return html; // return fully rendered HTML
+}
+
+console.log(ssr(url));
